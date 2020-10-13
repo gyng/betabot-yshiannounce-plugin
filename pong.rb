23fc95cacd2d01d394fc22f80be074c70e05846d
Ng Guoyou <gyng@users.noreply.github.com>
2020-10-13 21:39:58 +0800

    feat: Add settings example

--- pong.rb
+++ pong.rb
@@ -26,22 +26,33 @@ class Bot::Plugin::Pong < Bot::Plugin
     # You can add custom settings for the bot as a key in the settings object @s
 
     @s = {
-      trigger: { pong: [:method_to_call, 0, 'Pongs the bot.'] },
+      trigger: {
+        pong: [:method_to_call, 0, 'Pongs the bot.'],
+        change_reply: [:change_reply, 0, 'Change the pong reply message']
+      },
+      reply_with: "peng",
       subscribe: false
     }
 
     super(bot)
   end
 
-  def method_to_call(msg = nil)
-    msg.reply('peng')
+  def method_to_call(msg)
+    msg.reply(@s[:reply_with])
+  end
+
+  def change_reply(msg)
+    new_reply = msg.args[0]
+    @s[:reply_with] = new_reply
+    save_settings
+    msg.reply "Changed to #{new_reply}"
   end
 
   # The method called when an adapter is fully ready
   # Eg, when the bot has registered its nickname with the IRC server
   def on_connect(_adapter, _connection)
     # If IRC is addressable, send "Betabot is here!" to #mychannel on the IRC server group named "myserver"
-    @bot.address_str('irc:::myserver:::#mychannel')&.reply('Betabot is here!')
+    # @bot.address_str('irc:::myserver:::#mychannel')&.reply('Betabot is here!')
   end
 
   # This method receives published (=broadcasted) messages.
--- spec/pong_spec.rb
+++ spec/pong_spec.rb
@@ -4,11 +4,25 @@ require 'betabot' # loads as a lib for access to structures
 require_relative '../pong'
 
 describe Bot::Plugin::Pong do
-  subject { Bot::Plugin::Pong.new(nil) }
+  it 'changes the reply message' do
+    plugin = Bot::Plugin::Pong.new(nil)
 
-  it 'responds to pong with peng' do
     m = Bot::Core::Message.new
-    expect(m).to receive(:reply).with('peng')
-    subject.method_to_call(m)
+    m.text = 'trigger warrrgh'
+    expect(m).to receive(:reply).with("Changed to #{m.args[0]}")
+    plugin.change_reply(m)
+
+    m2 = Bot::Core::Message.new
+    expect(m2).to receive(:reply).with('warrrgh')
+    plugin.method_to_call(m2)
+
+    m3 = Bot::Core::Message.new
+    m3.text = 'trigger peng'
+    expect(m3).to receive(:reply).with("Changed to #{m3.args[0]}")
+    plugin.change_reply(m3)
+
+    m4 = Bot::Core::Message.new
+    expect(m4).to receive(:reply).with('peng')
+    plugin.method_to_call(m4)
   end
 end
